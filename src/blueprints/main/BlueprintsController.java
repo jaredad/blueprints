050914c4182d375f0aaa462c8b30e70d50653e28
Austin Jared <35081849+jaredad@users.noreply.github.com>
2018-06-20 08:59:53 -0500

    Add files via upload

---
+++ src/blueprints/main/BlueprintsController.java
@@ -0,0 +1,22 @@
+package blueprints.main;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+
+public class BlueprintsController {
+	
+	@FXML
+    private Tab fileTab, objectsTab, connectorsTab;
+	
+	@FXML
+	private TabPane tabPane;
+	
+	public void initialize() {
+		
+	}
+	
+
+}
